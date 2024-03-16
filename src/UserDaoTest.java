15bb131add19e29112fa952c6d13d9b2929bffed
Yeeun Chung <yesung00000@gmail.com>
2024-03-16 12:22:32 +0900

    1.3 DAO의 확장

---
+++ src/UserDaoTest.java
@@ -0,0 +1,24 @@
+import java.sql.SQLException;
+
+public class UserDaoTest {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ConnectionMaker connectionMaker = new DConnectionMaker();
+
+        UserDao dao = new UserDao(connectionMaker);
+
+        User user = new User();
+        user.setId("id1");
+        user.setName("양정인");
+        user.setPassword("lovely");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+}
